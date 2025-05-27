6809c4ee8e81a79cc06d9e98b7cdf3b82cc4a517
Sai krishna Basetti <saikrishnabasetti@MacBookPro.ve63-core1-shmntx.graysoncollin.net>
2025-05-27 08:18:47 -0500

    Changing region to central

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,7 +6,7 @@ variable "project_id" {
 variable "region" {
   description = "GCP Region"
   type        = string
-  default     = "us-south1"
+  default     = "us-central1"
 }
 
 variable "repo_name" {
